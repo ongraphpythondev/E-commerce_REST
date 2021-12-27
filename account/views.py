78bf3df238452b0032d953033b774e5ab82e4d7c
unknown <riteshpandey1200@gmail.com>
2021-12-27 18:54:51 +0530

    first commit

--- account/views.py
+++ account/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from twilio.rest import Client
 # Create your views here.
+
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        mobile = request.POST["mobile"]
+        print(mobile)
+        account_sid = "AC796220b91d3780f0783e40474c9dea67"
+        auth_token = "2da75293af496540050556c2358ebbf5"
+        client = Client(account_sid, auth_token)
+
+        message = client.messages \
+            .create(
+                body='This is the ship that made the Kessel Run in fourteen parsecs?',
+                from_='+17817904373',
+                to='+917060699351'
+            )
+
+        print(message.sid)
+
+    
+    return HttpResponse("done")
