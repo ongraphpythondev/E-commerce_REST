32eb8d5a47615cf34048af308eee357fa4235355
unknown <riteshpandey1200@gmail.com>
2021-12-28 17:55:21 +0530

    added env variale

--- account/views.py
+++ account/views.py
@@ -22,31 +22,37 @@ class Login(APIView):
         characters = string.ascii_letters + string.digits + string.punctuation
         password = ''.join(random.choice(characters) for i in range(8))
         
+        # it generate random 4 number for our otp
         otp=""
         for i in range(4):
             otp+=str(random.randint(1,9))
 
+        # create dict to send data
         data = {
             "mobile":request.data["mobile"],
             "password" : password,
             "otp" : otp
         }
+
         serializer = UserSerializer(data = data)
         if serializer.is_valid():
             serializer.save()
 
-            account_sid = os.environ.get("account_sid") 
-            auth_token = os.environ.get("auth_token") 
-            client = Client(account_sid, auth_token)
+            try:
+                # sending message to client
+                account_sid = os.environ.get("account_sid") 
+                auth_token = os.environ.get("auth_token") 
+                client = Client(account_sid, auth_token)
 
-            message = client.messages \
-                .create(
-                    body=f'Its from Ongraph your code is {otp}',
-                    from_='+17817904373',
-                    to='+91'+request.data["mobile"]
-                )
+                message = client.messages \
+                    .create(
+                        body=f'Its from Ongraph your code is {otp}',
+                        from_='+17817904373',
+                        to='+91'+request.data["mobile"]
+                    )
+            except: 
+                return Response({"error":"something went wrong please try again"}, status=status.HTTP_400_BAD_REQUEST)
 
-            print(message.sid)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -55,19 +61,25 @@ class Login(APIView):
 
 class Verify(APIView):
     def post(self, request , user_pk = None):
+        # getting otp
         otp = request.data["otp"]
+
+        # validation
         if user_pk == None:
             return Response({"error":"write pk"}, status=status.HTTP_400_BAD_REQUEST)
         if not otp:
             return Response({"error":"please enter otp"}, status=status.HTTP_400_BAD_REQUEST)
         
+        # User object
         user = CustomUser.objects.get(pk = user_pk)
         if not user:
             return Response({"error":"User not found"}, status=status.HTTP_400_BAD_REQUEST)
 
+        # checking the otp we received and otp in db is same 
         if user.otp != otp :
             return Response({"error":"Please enter correct otp"}, status=status.HTTP_400_BAD_REQUEST)
         
+        # user is verified
         user.is_verified = True
         user.save()
         return Response({"success":"User succesfully verified"}, status=status.HTTP_200_OK)
